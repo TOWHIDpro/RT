7b014a2085044600c2ef93d0e81e4031c7f27562
TOWHIDpro <sajjad.rk05@gmail.com>
2022-05-15 10:58:09 +0600

    public html

--- mtrajpathbdltd/settings.py
+++ mtrajpathbdltd/settings.py
@@ -160,10 +160,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/home/mtrajpat/public_html/static'
+STATIC_ROOT = '/home/public_html/static'
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = '/home/mtrajpat/public_html/media'
+MEDIA_ROOT = '/home/public_html/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
