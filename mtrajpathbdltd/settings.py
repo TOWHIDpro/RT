848549483a884be9d7640e6b5a26346baa2735d2
TOWHIDpro <sajjad.rk05@gmail.com>
2022-05-15 12:15:23 +0600

    static root change

--- mtrajpathbdltd/settings.py
+++ mtrajpathbdltd/settings.py
@@ -159,11 +159,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = '/home/public_html/static'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/mtrajpat/public_html/static'
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = '/home/public_html/media'
+MEDIA_ROOT = '/home/mtrajpat/public_html/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
